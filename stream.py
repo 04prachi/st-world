bf09cafbdccb2b4b1420841b311a08e545ff9ee9
04prachi <129326281+04prachi@users.noreply.github.com>
2024-03-24 01:56:43 +0530

    Update stream.py

--- stream.py
+++ stream.py
@@ -1,9 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-pip install scipy
-from scipy import stats
-from scipy.stats import norm
+#from scipy import stats
+#from scipy.stats import norm
 def perform_ab_test(control_visitors, control_conversions, treatment_visitors, treatment_conversions, confidence_level):
     # Calculate conversion rates for control and treatment groups
     control_conversion_rate = control_conversions / control_visitors
