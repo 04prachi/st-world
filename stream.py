f9ebe2594c07e6b599d79f661a4466830e64bea5
04prachi <129326281+04prachi@users.noreply.github.com>
2024-03-24 01:47:32 +0530

    Update stream.py

--- stream.py
+++ stream.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from scipy import stats
 from scipy.stats import norm
 def perform_ab_test(control_visitors, control_conversions, treatment_visitors, treatment_conversions, confidence_level):
